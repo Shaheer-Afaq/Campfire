86865bd626d6cc7164a533a36edfbd2f9ab5a9f4
Shaheer-Afaq <23121997doa@gmail.com>
2026-03-01 01:15:15 +0500

    .

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,35 +6,33 @@ const GRAVITY = 2000
 
 #func _ready() -> void:
 func _physics_process(delta: float) -> void:
-	#if is_dead:
-		#if $AnimatedSprite2D.frame == 28:
-			#Manager.lives -= 1
-			#get_tree().reload_current_scene()
-		#else: return
-		
+	velocity.x *= 0.89
+	
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
 	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = Manager.jump_velocity
 		
-	velocity.x *= 0.89
+	if Input.is_action_pressed("attack"):
+		$AnimatedSprite2D.play("attack")
+	
+	if not ($AnimatedSprite2D.animation == "attack" and $AnimatedSprite2D.is_playing()):
+		var direction := Input.get_axis("left", "right")
+		velocity.x += direction * Manager.speed * delta * 60  # frame-independent
 
-	var direction := Input.get_axis("left", "right")
-	velocity.x += direction * Manager.speed * delta * 60  # frame-independent
-
-		# Animations
-	if abs(velocity.x) > 30:
-		if not $footsteps.playing:
-			$footsteps.play(0.0)
-		if velocity.x > 0:
-			$AnimatedSprite2D.flip_h = false
+		if abs(velocity.x) > 30:
+			if not $footsteps.playing:
+				$footsteps.play(0.0)
+			if velocity.x > 0:
+				$AnimatedSprite2D.flip_h = false
+			else:
+				$AnimatedSprite2D.flip_h = true
+			
+			$AnimatedSprite2D.play("run")
 		else:
-			$AnimatedSprite2D.flip_h = true
-		$AnimatedSprite2D.play("run")
-	else:
-		$footsteps.stop()	
-		$AnimatedSprite2D.play("idle")
-
+			$footsteps.stop()	
+			$AnimatedSprite2D.play("idle")
+		
 	var collison = move_and_slide()
 	#var touching_floor = is_on_floor()
 	#var touching_ceiling = is_on_ceiling()
